613981cced56666255b44086ffe5b69d1e86c335
mariowibowor <mwroyro1@gmail.com>
2023-04-02 15:51:01 +0700

    Penambahan repositori

---
+++ src/main/java/sketsa/admin.java
@@ -0,0 +1,14 @@
+package sketsa;
+
+import java.util.Scanner;
+
+public class admin {
+    public static void mainadmin() {
+        cekhasil();
+    }
+
+    static void cekhasil() {
+        menu_utama main = new menu_utama();
+        main.hasil();
+    }
+}
