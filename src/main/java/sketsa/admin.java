ae11410c3f121cf8b888b1051dbe4e09bdaa99fd
mariowibowor <mwroyro1@gmail.com>
2023-04-02 16:01:49 +0700

    Penambahan sistem login admin

--- src/main/java/sketsa/admin.java
+++ src/main/java/sketsa/admin.java
@@ -3,7 +3,19 @@ package sketsa;
 import java.util.Scanner;
 
 public class admin {
+    public static String[] Design = {
+            "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
+            "*************************************************************************",
+            "-------------------------------------------------------------------------"};
+
     public static void mainadmin() {
+        String keyusername = "admin";
+        int keyPIN = 123;
+        String username = "";
+        int PIN = 0;
+        int pilihan;
+        int pilihan2 = 1;
+        boolean ceklogin = login(PIN, keyPIN, username, keyusername);
         cekhasil();
     }
 
@@ -22,5 +34,22 @@ public class admin {
         }
         scan = cek.nextInt();
         return scan;
+    }
+
+    static boolean login(int PIN, int keypin, String username, String keyusername) {
+        System.out.println(Design[2] + "\nSelamat Datang Administrator");
+        while (true) {
+            System.out.println(Design[2] + "\nSilahkan masukkan username dan PIN");
+            System.out.print("Username: ");
+            Scanner user = new Scanner(System.in);
+            username = user.nextLine();
+            System.out.print("PIN: ");
+            Scanner pin = new Scanner(System.in);
+            PIN = ceknomor(pin, "PIN: ");
+
+            if (PIN == keypin && username.equals(keyusername)) {
+                return true;
+            }
+        }
     }
 }
